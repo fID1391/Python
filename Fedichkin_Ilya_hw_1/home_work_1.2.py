f316fbb4718a02880060ca38d53cbabfe7aad96b
fID1391 <oliono1391@gmail.com>
2022-05-24 13:37:22 +0300

    lesson_1 use fuction

--- Fedichkin_Ilya_hw_1/home_work_1.2.py
+++ Fedichkin_Ilya_hw_1/home_work_1.2.py
@@ -3,33 +3,21 @@
 # Основы языка Python
 # Lesson 1 task 2
 
-cube_list = []
-summ_var_a = 0
-summ_var_b = 0
+def funct(arg):
+    global_summ = 0
+    for i in range(len(arg)):
+        some_variable = arg[i]
+        summ_of_numbers = 0
+        while some_variable > 0:
+            summ_of_numbers = summ_of_numbers + some_variable % 10
+            some_variable = some_variable // 10
+        if summ_of_numbers % 7 == 0:
+            global_summ = global_summ + arg[i]
+    return (global_summ)
 
-# Вариант а)
-for cube in range(1, 1001, 2):
-    cube_list.append(cube**3)
-for i in range(len(cube_list)):
-    number = cube_list[i]
-    summ = 0
-    while number > 0:
-        num = number % 10
-        number = number // 10
-        summ += num
-    if (summ % 7) == 0:
-        summ_var_a += cube_list[i]
-print(summ_var_a)
+cube_list = list(i**3 for i in range(1, 1001, 2))
+print(funct(cube_list))
 
-# Вариант б) со одной зведочкой
 for i in range(len(cube_list)):
     cube_list[i] += 17
-    summ = 0
-    number = cube_list[i]
-    while number > 0:
-        num = number % 10
-        number = number // 10
-        summ += num
-    if (summ % 7) == 0:
-        summ_var_b += cube_list[i]
-print(summ_var_b)
+print(funct(cube_list))
